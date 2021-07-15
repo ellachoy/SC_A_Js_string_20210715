2a491a233095d46fece3280b0cdc76982883f68d
Ella Choy <egal21de@gmail.com>
2021-07-15 18:02:38 +0200

    finito

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+let x="Hello"
+let y="World<br>"
+console.log(x,y)
+
+
+// 1.Addiere x mit y und speichere das Ergebnis als Variable z;
+// Nutze den Befehl document.write() mit Wert z zwischen Klammern um im Dokument Werte anzuzeigen. Es wird im HTML Dokument angezeigt.
+
+ let z=x+y
+ document.write(z);
+
+
+// 2.   Schreibe x + " " + y und speichere alles in der Variable n;
+//      Nutze den Befehl document.write() mit Wert n;
+
+let n=x + " "+y
+document.write(n)
+
+// 3.   Addiere x und String "World" mit dem passenden Zuweisungsoperator.
+
+x += "World";
+
+// 4. Nutze den Befehl document.write() mit Wert x den String im Dokument auszugeben;
+
+document.write(x)
